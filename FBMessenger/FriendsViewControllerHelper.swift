5817ba9ade8daa0f560497d3adf94936c23885cb
Yeontae Kim <ytk007@gmail.com>
2018-02-01 13:20:44 -0800

    fetching seed data using core data

--- FBMessenger/FriendsViewControllerHelper.swift
+++ FBMessenger/FriendsViewControllerHelper.swift
@@ -9,9 +9,47 @@
 import UIKit
 import CoreData
 
+enum MessagesResult {
+    case success([Message])
+    case failure(Error)
+}
+
 extension FriendsViewController {
     
+    func clearData() {
+        
+        let context = self.coreDataStack.managedContext
+        
+        let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Friend")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetch)
+            
+        do {
+            try context.execute(deleteRequest)
+            try context.save()
+        } catch let error {
+            print (error)
+        }
+        
+    }
+    
+    func loadData(completion: @escaping (MessagesResult) -> Void) {
+        
+        let fetchRequest: NSFetchRequest<Message> = Message.fetchRequest()
+        let moc = coreDataStack.managedContext
+        
+        moc.perform {
+            do {
+                let messages = try moc.fetch(fetchRequest)
+                completion(.success(messages))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
     func setupData() {
+        
+        clearData()
         
         let context = self.coreDataStack.managedContext
         
@@ -34,10 +72,26 @@ extension FriendsViewController {
         message2.date = NSDate()
         message2.friend = steve
         
-        messages = [message1, message2]
+        do {
+            try context.save()
+        } catch let err {
+            print(err)
+        }
+        
+
+        loadData() { (messagesResult) in
+            
+            switch messagesResult {
+                
+            case let .success(messages):
+                self.messages = messages
+                self.collectionView?.reloadData()
+                
+            case .failure(_):
+                self.messages = []
+                
+            }
+        }
+
     }
-    
-    
-    
-    
 }
